2161943e5b493134fa85f986f318fc15641d2ef0
atmoner <contact.atmoner@gmail.com>
2024-12-24 19:36:10 +0100

    Send doc

---
+++ .vitepress/config.mjs
@@ -0,0 +1,109 @@
+import { defineConfig } from 'vitepress'
+import { withMermaid } from "vitepress-plugin-mermaid";
+ 
+
+// https://vitepress.dev/reference/site-config
+// export default defineConfig({
+export default withMermaid({
+  base: process.env.NODE_ENV === "production" ? "/unicosm-doc/" : "/",
+  title: "Unicosm",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: '/img/UnicosmLogo.png',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Documentation', link: '/scripts/CosmosChainConfig.cs' },
+      //{ text: 'Scripts', link: '/scripts/CosmosChainConfig.cs' },
+      //{ text: 'Team', link: '/team' },
+      /*{
+        text: 'Version 1.0.1',
+        items: [
+          { text: '1.0.1', link: '/quickstart/' } 
+        ]
+      }*/
+    ],
+    footer: {
+      //message: 'Released under the <a href="https://github.com/vuejs/vitepress/blob/main/LICENSE">MIT License</a>.',
+      copyright: 'Copyright © 2025 <a href="https://github.com/cosmDev">CosmDev</a>'
+    },
+    sidebar: [
+      {
+        text: 'Introduction',
+        link: '/quickstart/',
+        collapsed: true,
+        items: [
+          { text: '💡 What is Unicosm?', link: '/quickstart/' } 
+        ]
+      },
+      {
+        text: 'Quick Start', 
+        link: '/quickstart/installation',
+        collapsed: true,
+        items: [
+          { text: '⚒️ Package Installation', link: '/quickstart/installation' },
+          { text: '⚙️ WebGL configuration', link: '/quickstart/webgl-configuration' },
+          { text: '👉 Basic use', link: '/quickstart/basic-use' },
+          { text: '🦾 Advanced use', link: '/quickstart/advanced-use' }
+        ]
+ 
+      },
+      {
+        text: 'Tutorials',
+        link: '/tutorials/starter-pack',
+        collapsed: true,
+        items: [
+          { text: '🟢 Starter pack', link: '/tutorials/starter-pack' },
+          { text: '🟢 Cosmos Login', link: '/tutorials/cosmos-login' },
+          { text: '🟢 Cosmos Sign Arbitrary', link: '/tutorials/cosmos-sign-arbitrary' },
+          { text: '🟢 Cosmos Send Token', link: '/tutorials/cosmos-send-token' },
+          { text: '🟢 CosmWasm Query', link: '/tutorials/cosmwasm-query' },
+          { text: '🟡 CosmWasm Execute', link: '/tutorials/cosmwasm-execute' }
+        ]
+      },
+      {
+        text: 'Advanced Tutorials',
+        link: '/advanced-tutorials/ceate-your-smartcontract',
+        collapsed: true,
+        items: [
+          { text: '🟡 Create your smartcontract', link: '/advanced-tutorials/ceate-your-smartcontract' },
+          { text: '🟡 Smartcontract logic', link: '' } 
+        ]
+      },
+      {
+        text: 'UniCosm Scripts', 
+        link: '/scripts/CosmosChainConfig.cs',
+        collapsed: true,
+        items: [
+          { text: '🟢 CosmosChainConfig.cs', link: '/scripts/CosmosChainConfig.cs' },
+          { text: '🟢 CosmosSignerConnect.cs', link: '/scripts/CosmosSignerConnect.cs' },
+          { text: '🟢 CosmosSignArbitrary.cs', link: '/scripts/CosmosSignArbitrary.cs' },
+          { text: '🟢 CosmosQueryAccount.cs', link: '/scripts/CosmosQueryAccount.cs' },
+          { text: '🟢 CosmosSendToken.cs', link: '/scripts/CosmosSendToken.cs' },
+          { text: '🟢 CosmosDebugServer.cs', link: '/scripts/CosmosDebugServer.cs.md' }
+        ]
+      },
+      {
+        text: 'CosmWasm scripts',
+        link: '/scripts/CosmosCosmWasmQuery.cs',
+        collapsed: true,
+        items: [
+          { text: '🟢 CosmWasmQuery.cs', link: '/scripts/CosmosCosmWasmQuery.cs' },
+          { text: '🟢 CosmWasmExecute.cs', link: '/scripts/CosmosCosmWasmExecute.cs' }
+        ]
+      }
+    ],
+    search: {
+      provider: 'local'
+    },
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/cosmDev' },
+      //{ icon: 'twitter', link: '...' },
+    ]
+  },
+  mermaid:{
+    //mermaidConfig !theme here works for ligth mode since dark theme is forced in dark mode
+  },
+  
+})
+ 
---
+++ .vitepress/theme/index.js
@@ -0,0 +1,30 @@
+// https://vitepress.dev/guide/custom-theme
+import { h } from 'vue'
+import DefaultTheme from 'vitepress/theme'
+import './style.css'
+//import 'animate.css';
+
+import imageViewer from 'vitepress-plugin-image-viewer';
+import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
+import { useRoute } from 'vitepress';
+
+/** @type {import('vitepress').Theme} */
+export default {
+  extends: DefaultTheme,
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    })
+  },
+  enhanceApp(ctx) {
+    DefaultTheme.enhanceApp(ctx);
+    // Register global components, if you don't want to use it, you don't need to add it
+    ctx.app.component('vImageViewer', vImageViewer);
+  },
+  setup() {
+    // Get route
+    const route = useRoute();
+    // Using
+    imageViewer(route);
+  }
+}
